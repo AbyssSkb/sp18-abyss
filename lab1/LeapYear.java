ca7f8b062e84c07685f14d06b8768f724733ec23
AbyssSkb <abyssskb@outlook.com>
2025-07-01 08:57:43 +0800

    completed the rest of lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -2,6 +2,10 @@
  *  @author YOUR NAME HERE
  */
 public class LeapYear {
+    /** check if a year is leap year. */
+    public static boolean isLeapYear(int year) {
+       return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
